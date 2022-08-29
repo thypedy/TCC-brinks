76b3b22dc4015ea2cc18713d598fb9766f755157
thypedy <84384293+thypedy@users.noreply.github.com>
2022-08-29 10:16:42 -0300

    Add files via upload

---
+++ config/servidor.js
@@ -0,0 +1,18 @@
+//importar o pacote express
+const express = require('express')
+//executar o express application
+const app = express()
+
+//configurar a porta remota e local
+const porta = process.env.PORT || 3000
+
+//importar o pacote consign
+const consign = require('consign')
+//executar e configurar o consign
+consign().include('./routes').into(app)
+
+//definir a pasta dos assets (css, images, js)
+app.use(express.static('./src/'))
+
+//definir os conteudos com acesso externo
+module.exports = {app, porta}
